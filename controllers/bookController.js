1ffd9f1ec272a290fb2975281664f28b4078aa06
Sainath-Nanaware <sainathnanaware2001@gmail.com>
2025-05-27 15:31:17 +0530

    complete get bookByID and search api

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,4 +1,6 @@
 const Book=require('../models/bookModel')
+const Review=require('../models/reviewModel')
+const mongoose=require('mongoose')
 const {
   successResponse,
   errorResponse,
@@ -42,4 +44,70 @@ exports.getAllBooks=async(req,resp)=>{
         errorResponse(resp, "Internal server error", 500, error);
         console.log(error);
     }
+}
+
+exports.getBookById=async(req,resp)=>{
+    const {id}=req.params
+      const { page = 1, limit = 5 } = req.query;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return errorResponse(resp, "invalid review id", 401);
+    }
+    try{
+        const bookInfo=await Book.findById(id)
+        if(!bookInfo){
+            return errorResponse(resp,"book not found",401)
+        }
+         const reviews = await Review.find({ bookId: id })
+           .skip((page - 1) * limit)
+           .limit(Number(limit))
+           .populate("userId", "name"); // Optional: show user names
+
+         const avgRatingData = await Review.aggregate([
+           { $match: { bookId: new mongoose.Types.ObjectId(id) } },
+           { $group: { _id: "$bookId", avgRating: { $avg: "$rating" } } },
+         ]);
+
+         const averageRating = avgRatingData[0]?.avgRating || 0;
+
+         const data={
+           bookInfo,
+           averageRating: averageRating.toFixed(2),
+           reviews,
+           pagination: {
+             page: Number(page),
+             limit: Number(limit),
+           },
+         };
+         successResponse(resp,data,"book info",200)
+
+    }catch(error){
+        errorResponse(resp, "Internal server error", 500, error);
+        console.log(error);
+    }
+}
+
+
+exports.search=async(req,resp)=>{
+    const {q}=req.query
+    console.log("in search");
+    
+    if(!q || q.trim()===''){
+        return errorResponse(resp,"search query is required",400)
+    }
+    try{
+        const searchRegex=new RegExp(q,'i')
+        console.log("serach Regex=",searchRegex);
+        
+        const books = await Book.find({
+          $or: [
+            { title: { $regex: searchRegex } },
+            { author: { $regex: searchRegex } },
+          ],
+        });
+        successResponse(resp,books,"filter books",200)
+    }catch(error){
+        errorResponse(resp, "Internal server error", 500, error);
+        console.log(error);
+    }
+
 }
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -3,10 +3,17 @@ const router=express.Router()
 const auth=require('../middlewares/auth')
 const{addBookSchema} =require('../validations/bookValidation')
 const validate=require('../middlewares/schemaValidate')
-const { addBook, getAllBooks } = require("../controllers/bookController");
+const {
+  addBook,
+  getAllBooks,
+  getBookById,
+  search
+} = require("../controllers/bookController");
 
 router.post('/',auth,validate(addBookSchema),addBook)
 router.get('/books',auth,getAllBooks)
+router.get("/search",auth,search);
+router.get("/:id", auth, getBookById);
 
 
 module.exports=router
